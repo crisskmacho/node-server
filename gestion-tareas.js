753b4cd03794f48c833170222377a7955cad3b82
cristian camacho <cristianduvang12@hotmail.com>
2023-09-29 17:17:44 -0500

    agregar funcion askForAction

--- gestion-tareas.js
+++ gestion-tareas.js
@@ -10,6 +10,7 @@ const tasks = [];
 function addTask(indicator, description) {
   tasks.push({ indicator, description, completed: false });
   console.log(`Tarea "${indicator}: ${description}" agregada.`);
+  askForAction(); // Llamar a la función para preguntar nuevamente
 }
 
 function deleteTask(indicator) {
@@ -20,6 +21,7 @@ function deleteTask(indicator) {
   } else {
     console.log(`No se encontró una tarea con el indicador "${indicator}".`);
   }
+  askForAction(); // Llamar a la función para preguntar nuevamente
 }
 
 function completeTask(indicator) {
@@ -30,6 +32,7 @@ function completeTask(indicator) {
   } else {
     console.log(`No se encontró una tarea con el indicador "${indicator}".`);
   }
+  askForAction(); // Llamar a la función para preguntar nuevamente
 }
 
 function listTasks() {
@@ -38,36 +41,34 @@ function listTasks() {
     const status = task.completed ? 'Completada' : 'Pendiente';
     console.log(`- ${task.indicator}: ${task.description} (${status})`);
   });
+  askForAction(); // Llamar a la función para preguntar nuevamente
 }
 
-rl.question('¿Qué acción deseas realizar? (add/delete/complete/list/exit): ', action => {
-  if (action === 'add') {
-    rl.question('Indicador de la tarea: ', indicator => {
-      rl.question('Descripción de la tarea: ', description => {
-        addTask(indicator, description);
-        listTasks();
-        rl.close();
+function askForAction() {
+  rl.question('¿Qué acción deseas realizar? (add/delete/complete/list/exit): ', action => {
+    if (action === 'add') {
+      rl.question('Indicador de la tarea: ', indicator => {
+        rl.question('Descripción de la tarea: ', description => {
+          addTask(indicator, description);
+        });
       });
-    });
-  } else if (action === 'delete') {
-    rl.question('Indicador de la tarea a eliminar: ', indicator => {
-      deleteTask(indicator);
+    } else if (action === 'delete') {
+      rl.question('Indicador de la tarea a eliminar: ', indicator => {
+        deleteTask(indicator);
+      });
+    } else if (action === 'complete') {
+      rl.question('Indicador de la tarea a marcar como completada: ', indicator => {
+        completeTask(indicator);
+      });
+    } else if (action === 'list') {
       listTasks();
+    } else if (action === 'exit') {
       rl.close();
-    });
-  } else if (action === 'complete') {
-    rl.question('Indicador de la tarea a marcar como completada: ', indicator => {
-      completeTask(indicator);
-      listTasks();
-      rl.close();
-    });
-  } else if (action === 'list') {
-    listTasks();
-    rl.close();
-  } else if (action === 'exit') {
-    rl.close();
-  } else {
-    console.log('Acción no válida.');
-    rl.close();
-  }
-});
+    } else {
+      console.log('Acción no válida.');
+      askForAction(); // Llamar a la función para preguntar nuevamente
+    }
+  });
+}
+
+askForAction();
