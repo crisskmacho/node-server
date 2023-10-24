25d5cd2f5d42ac29adec2828db6dec9022103f34
cristian camacho <cristianduvang12@hotmail.com>
2023-10-23 22:09:45 -0500

    Autenticación en Express

--- gestion-tareas.js
+++ gestion-tareas.js
@@ -6,6 +6,72 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+//constates jwt y dotenv
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+
+
+dotenv.config();
+
+
+// Usuarios predefinidos
+const usuarios = [
+  { username: 'usuario1', password: 'contrasena1' },
+  { username: 'usuario2', password: 'contrasena2' },
+];
+
+app.use(express.json());
+
+const jwtSecret = process.env.JWT_SECRET;
+
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+
+  //verificar las credenciales del usuario
+  const usuario = usuarios.find((user) => user.username === username && user.password === password);
+  if (!usuario) {
+    return res.status(401).json({ error: 'Credenciales incorrectas'});
+  }
+
+  //Generar el token JWT
+  const token = jwt.sign({ username}, jwtSecret, { expiresIn: '1h'});
+  //console.log("Generated token:", token);
+  res.json({ token });
+});
+
+
+//Middleware para verificar el token
+const verifyToken = (req, res, next) => {
+  const token = req.get("Authorization")
+
+  if (!token) {
+    res.status(401).json({ error: 'Token no proporcionado' });
+    return;
+  }
+
+  const tokenWithoutBearer = token.replace("Bearer ", "");
+
+  jwt.verify(tokenWithoutBearer, jwtSecret, (error, decoded) => {
+    if (error) {
+      res.status(401).json({ error: 'Token invalido' });
+      return;
+    }
+    req.user = decoded;  // Agrega el usuario autenticado a la solicitud para su posterior uso
+    next();
+  });
+  /*console.log(token);
+  console.log(jwt.verify(token, jwtSecret));*/
+}
+
+
+//Ruta protegida con el middleware verifyToken
+app.post('/rutaProtegida', verifyToken, (req, res) => {
+  res.status(200).json({ message: 'Ruta protegida' });
+});
+
+
+
+
 //constantes para los routers
 const listViewRouter = require('./list-view-router');
 const listEditRouter = require('./list-edit-router');
